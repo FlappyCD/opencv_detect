936430f0ce13e18b5d4236f2e25f1b6dc02fd55f
Chad.Cheng <495044939@qq.com>
2017-03-15 13:47:18 +0800

    11
    11

--- 源.cpp
+++ 源.cpp
@@ -8,7 +8,7 @@ using namespace cv;
 using namespace std;
 
 
-float max_radius,first_max_radius; Point2f max_center;//ROI参数
+float max_radius, first_max_radius; Point2f max_center;//ROI参数
 float sec_radius; Point2f sec_center;//内轮廓为旋转中心
 
 
@@ -27,15 +27,15 @@ void Getcontours(InputArray src, OutputArray dst)
 	vector<float> radius(contours.size());
 
 	vector <vector<Point>> contours_poly(contours.size());
-	
+
 	vector<float> temp(contours.size());
 	//画轮廓
 	Mat drawing = Mat::zeros(src_gray.size(), CV_8UC3);
 	for (int i = 0; i< contours.size(); i++)
 	{
-		approxPolyDP(Mat(contours[i]),contours_poly[i],3,true);//逼近曲线，应该要调整
+		approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true);//逼近曲线，应该要调整
 		minEnclosingCircle(contours_poly[i], center[i], radius[i]);
-		
+
 	}
 
 	//初始化，我们得到最小外轮廓外接圆来确定roi
@@ -51,11 +51,11 @@ void Getcontours(InputArray src, OutputArray dst)
 		//最大轮廓的外接圆ROI参数
 		if (max_radius < radius[i])
 		{
-	     	max_radius = radius[i];
+			max_radius = radius[i];
 			max_center = center[i];
 		}
 	}
-	circle(drawing,max_center, (int)max_radius, Scalar(0, 255, 255), 2, 8, 0);
+	circle(drawing, max_center, (int)max_radius, Scalar(0, 255, 255), 2, 8, 0);
 
 	sort(temp.begin(), temp.end());
 	for (int i = 0; i < contours.size(); i++)
@@ -116,52 +116,64 @@ Mat rotateImage2(Mat img, int degree, Point2f g_center)
 	warpAffine(img, img_rotate, map_matrix, Size(width_rotate, height_rotate), 1, 0, 0);
 	return img_rotate;
 }
+
+
+
 int main()
 {
 	Mat img = imread("222.jpg", 1);
 	Mat rotate_maps[15]; Mat ImageROIS[15];//旋转图形和旋转ROI
+	Mat diffcv;
 	Mat temp_rotate;//旋转之后的ROI区域暂存
-	Mat gray_img, dst_img, dst_contours,temp_contours,real_no_use;//原图像轮廓的相关应用变量
+	Mat gray_img, dst_img, dst_contours, temp_contours, real_no_use;//原图像轮廓的相关应用变量
 	//下面直接用自动的阈值操作来除噪
 	cvtColor(img, gray_img, COLOR_RGB2GRAY);//原始灰度图
-	threshold(gray_img,dst_img,150,255,3);//dst_img阈值之后的二值化图
-	
-	Getcontours(dst_img,dst_contours);//这个调用的作用是获取两个center和两个radius对图像没有任何影响
+	threshold(gray_img, dst_img, 140, 255, 3);//dst_img阈值之后的二值化图
+
+	Getcontours(dst_img, dst_contours);//这个调用的作用是获取两个center和两个radius对图像没有任何影响
 	first_max_radius = max_radius;
 
 	Mat temp = dst_img;
 	//下面来获取赶兴趣区域
-	Mat imageROI; 
+	Mat imageROI;
+	circle(temp, max_center, (int)max_radius, Scalar(255, 255, 255), 2, 8, 0);
 	imageROI = temp(Rect(max_center.x - max_radius, max_center.y - max_radius, max_radius * 2, max_radius * 2));//最小正方形区域
-	
+
 	vector<vector<Point> > contours;
 	vector<Vec4i> hierarchy;
 
 	//查找轮廓，并填充
 	findContours(imageROI, contours, hierarchy, RETR_EXTERNAL, CV_CHAIN_APPROX_NONE, Point(0, 0));
-	drawContours(imageROI, contours,-1,(255,255,255),FILLED,8,hierarchy);
+	drawContours(imageROI, contours, -1, (255, 255, 255), FILLED, 8, hierarchy);
 
-	
 
-	circle(temp, sec_center, (int)sec_radius, Scalar(0, 255, 255), 2, 8, 0);
-	circle(temp, sec_center, 4, Scalar(0, 255, 255), -1, 8, 0);
-		
+
+	//circle(temp, sec_center, (int)sec_radius, Scalar(0, 255, 255), 2, 8, 0);
+	//circle(temp, sec_center, 4, Scalar(0, 255, 255), -1, 8, 0);
+
 
 	namedWindow("result", WINDOW_NORMAL);
 	imshow("result", temp);
-	
-	
+
+
 	//转过一次之后,那我们再来对此去最小ROI就可以避免出现大小不一的情况了
-    
+
 
 	rotate_maps[0] = imageROI.clone();
-	
+
 
 
 	//接下来用sec_center为中心来进行旋转24度*15次=360度
 	for (int i = 1; i < 15; i++)
 	{
-		temp_contours = rotateImage2(imageROI, 24*i, sec_center);
+		if (i < 8)
+		{
+			temp_contours = rotateImage2(imageROI, 24 * i, sec_center);
+		}
+		else
+		{
+			temp_contours = rotateImage2(imageROI, -24 * i, sec_center);
+		}
 		temp_rotate = temp_contours.clone();
 		Getcontours(temp_rotate, real_no_use);
 		ImageROIS[i] = temp_rotate(Rect(max_center.x - first_max_radius, max_center.y - first_max_radius, first_max_radius * 2, first_max_radius * 2)).clone();//最小正方形区域
@@ -169,7 +181,7 @@ int main()
 	}
 
 	Mat average = Mat::zeros(imageROI.size(), CV_32F); //累加和存储
-	
+
 	accumulate(rotate_maps[0], average);
 
 	for (int i = 1; i < 15; i++)
@@ -178,11 +190,22 @@ int main()
 	}
 	average = average / 15;
 
-	threshold(average, average, 150, 255, 3);
-	imwrite("average.jpg", average);
-	namedWindow("average", WINDOW_NORMAL);
-	imshow("average", average);
+	//threshold(average, average,150, 255, 3);
 
+
+	diffcv = Mat::zeros(imageROI.size(), CV_32F);
+	imageROI.convertTo(imageROI, CV_32F);
+	average.convertTo(average, CV_32F);
+
+	diffcv = imageROI - average;
+
+	imwrite("diffv.jpg", diffcv);
+
+	threshold(diffcv, diffcv, 200, 255, 3);
+
+
+	namedWindow("diff", WINDOW_NORMAL);
+	imshow("diff", diffcv);
 	waitKey(0);
 
 
